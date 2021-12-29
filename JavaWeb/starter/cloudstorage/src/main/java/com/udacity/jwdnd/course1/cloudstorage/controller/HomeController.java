83e7accb63b3fef1bbd65558ee27490d9bc69f68
OmarElhusseini <95829813+OmarElhusseini@users.noreply.github.com>
2021-12-29 23:08:30 +0100

    Add files via upload

---
+++ JavaWeb/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -0,0 +1,69 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+import java.sql.SQLException;
+
+@Controller
+@RequestMapping
+public class HomeController {
+    private UserService userService;
+    private FileService fileService;
+    private NoteService noteService;
+    private CredentialService credentialService;
+
+    public HomeController(UserService userService, FileService fileService, NoteService noteService, CredentialService credentialService) {
+        this.userService = userService;
+        this.fileService = fileService;
+        this.noteService = noteService;
+        this.credentialService = credentialService;
+    }
+
+    @GetMapping("/home")
+    public String homeView(Model model, Authentication authentication) throws Exception {
+        Integer userId = userService.getUserIdByUsername(authentication.getName());
+        model.addAttribute("filesUploaded",fileService.getFiles(userId));
+        model.addAttribute("notes", noteService.getAllNotes(userId));
+        model.addAttribute("allCredentials", credentialService.getAllCredentials(userId));
+        return "/home";
+    }
+
+    @PostMapping("/home")
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile fileUpload, Model model, Authentication authentication) throws Exception {
+        String uploadError = null;
+        Integer userId = userService.getUserIdByUsername(authentication.getName());
+        if (!fileService.isFileNameAvailable(fileUpload.getOriginalFilename(), userId)){
+            uploadError = "File with the same name was uploaded before";
+        }
+        if (uploadError == null){
+            int rowsAdded = fileService.uploadNewFile(fileUpload, userId);
+            if (rowsAdded < 0){
+                uploadError = "An error happened while uploading the File, pls try again!";
+            }
+            if (rowsAdded== 0){
+                uploadError = "there is no file to upload, pls choose file!";
+            }
+        }
+        if (!(uploadError == null)){
+            model.addAttribute("uploadError", uploadError);
+            return "/home";
+        }else {
+            model.addAttribute("success", true);
+            return "/result";
+        }
+    }
+}
---
+++ JavaWeb/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -0,0 +1,67 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class User {
+    private Integer userId;
+    private String username;
+    private String salt;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(Integer userId, String salt, String firstName, String lastName, String username, String password) {
+        this.userId = userId;
+        this.salt = salt;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
